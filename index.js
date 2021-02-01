b463a305452e373ef5d530cf69da714cba092e52
DiegoDominguez <ddominguez@stx.com.mx>
2021-02-01 00:09:14 -0600

    endpoint con GET y parámetros

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 'use strict'
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-    console.log('APIREST corriendo en http://localhost:3000');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/hola/:name', (req, resp) => {
+    resp.send({
+        message: `Hola ${ req.params.name }!`
+    })
+});
+
+app.listen(port, () => {
+    console.log(`APIREST corriendo en http://localhost:${ port }`);
 });
